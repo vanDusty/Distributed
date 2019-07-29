9ec22f33704904c69c34f6d1aeaca933a65a7af0
Van <1251787298@qq.com>
2019-07-29 23:24:33 +0800

    config init

---
+++ blogConfig/source/categories/index.md
@@ -0,0 +1,5 @@
+---
+title: 分类
+date: 2019-07-09 10:51:37
+type: "categories"
+---
---
+++ files/百度统计.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?ddcd15db9f34dd2fff4f8efe4962cd1c";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+</script>
